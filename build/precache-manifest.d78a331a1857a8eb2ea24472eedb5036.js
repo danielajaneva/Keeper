00ebc8a04cfc7c1c2d7c27f03ada28d13f29a532
danielajaneva <danielajaneva.mail@gmail.com>
2021-04-21 13:46:56 +0200

    Dependences

---
+++ build/precache-manifest.d78a331a1857a8eb2ea24472eedb5036.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bc6b0bb1e730dd0b4bfd064e24e09b",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "bbeeb640dcee0bfc739b",
+    "url": "/Keeper/static/js/2.f9a98a1c.chunk.js"
+  },
+  {
+    "revision": "8a2f56557a2e24e0bff7",
+    "url": "/Keeper/static/js/main.636fa081.chunk.js"
+  },
+  {
+    "revision": "c2cd02bec1164cebc775",
+    "url": "/Keeper/static/js/runtime-main.8d08ef39.js"
+  }
+]);
